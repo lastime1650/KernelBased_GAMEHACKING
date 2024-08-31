f6c872a81274a7673b43636e269379e158e1b923
Cominam <54367564+lastime1650@users.noreply.github.com>
2024-08-31 17:28:11 +0900

    Add files via upload

--- IOCTL_USERMODE/_NEW_MFC_/resource.h
+++ IOCTL_USERMODE/_NEW_MFC_/resource.h
@@ -2,7 +2,6 @@
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
 // MFCGAMEHACK.rc에서 사용되고 있습니다.
 //
-#define IDC_DUMP_PROCESS_ID_BUTTON1     2
 #define IDC_DUMP_PROCESS_ID_BUTTON      2
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
@@ -12,6 +11,10 @@
 #define Main_Menu                       130
 #define IDD_ATTACH_PROCESS              131
 #define IDD_DUMP_PROCESS                133
+#define IDD_MEMORY_VIEWER               135
+#define IDD_MEMORY_PAGE_SCAN            139
+#define IDD_SCAN_WINDOW                 141
+#define IDD_KERNEL_DLL_INJECTION        143
 #define IDC_LIST_Address_Table          1000
 #define EDIT_ADDRESS_INPUT              1001
 #define CHECK_BOX_for_Input_Address_or_Offset 1002
@@ -22,22 +25,52 @@
 #define EDIT_Attach_PID                 1010
 #define LIST_CTRL_ATTACH_PROCESS_ID     1010
 #define EDIT_Attach_ImageName           1011
-#define IDC_DUMP_PROCESS_OUTPUT_LIST    1011
 #define IDC_DUMP_SAVE_PATH_EDIT         1012
 #define IDC_DUMP_PROCESS_ID_EDIT        1013
+#define IDC_MEMORY_VIEW                 1016
+#define Control_Panel_Set_Scan_Size_ID  1018
+#define IDC_BUTTON1                     1019
+#define IDC_EDIT3                       1020
+#define IDC_COMBO1                      1021
+#define IDC_NextScan_Button             1021
+#define IDC_BUTTON2                     1022
+#define Control_Panel_Set_Address_ID    1023
+#define IDC_BUTTON3                     1024
+#define ATTACHED_TEXT_EDIT              1025
+#define IDC_EDIT4                       1027
+#define MEM_PAGE_SCAN_LIST_CONTROL_ID   1028
+#define PID_BUTTON                      1029
+#define IDC_CHECK1                      1031
+#define Left_List_Control               1032
+#define Below_List_Control              1033
+#define INPUT_text_search_edit          1034
+#define IDC_COMBO2                      1035
+#define IDC_FirstScan_Button            1036
+#define IDC_COMBO3                      1037
+#define Check_is_HEX                    1038
+#define IDC_DLL_ABS_PATH_EDIT           1039
+#define IDC_PID_EDIT                    1040
+#define IDC_FILE_CHOOSE_BUTTT           1041
+#define IDC_CHOOSE_LOADLIBRARY_COMBO    1042
+#define IDC_INJECT_BUU                  1043
 #define ID_ATTACH_STARTATTACH           32771
 #define ID_MEMORY_STARTMEMORYSCAN       32772
 #define ID_DUMP_PROCESSDUMP             32773
 #define ID_HELP_HOWTOUSEIT              32774
 #define ID_FILE                         32775
+#define ID_FUNCTIONS                    32776
+#define ID_MEMORY_OPEN                  32777
+#define ID_MEMORY_OPEN32778             32778
+#define ID_FUNCTIONS_KERNEL             32779
+#define ID_FUNCTIONS_HARDWARE           32780
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        135
-#define _APS_NEXT_COMMAND_VALUE         32776
-#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_RESOURCE_VALUE        145
+#define _APS_NEXT_COMMAND_VALUE         32781
+#define _APS_NEXT_CONTROL_VALUE         1044
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
