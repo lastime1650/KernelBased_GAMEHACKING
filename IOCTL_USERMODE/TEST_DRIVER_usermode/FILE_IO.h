a9c45429ba9d230ad59a27c5ed78580af1d99e83
Cominam <54367564+lastime1650@users.noreply.github.com>
2024-09-02 12:35:43 +0900

    Delete IOCTL_USERMODE/TEST_DRIVER_usermode directory

--- IOCTL_USERMODE/TEST_DRIVER_usermode/FILE_IO.h
+++
@@ -1,9 +0,0 @@
-#ifndef  FILE_IO
-#define FILE_IO
-
-#include <stdio.h>
-#include <Windows.h>
-
-BOOLEAN File_Create(PUCHAR INPUT_DATA, ULONG32 INPUT_DATA_SIZE, LPCSTR FILE_PATH);
-
-#endif // ! FILE_IO
